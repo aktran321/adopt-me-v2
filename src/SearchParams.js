d372739b68b0f4542857ae1718bad14541dadfa0
Andy Tran <aktran321@gmail.com>
2020-01-28 08:12:30 -0500

    made custom hook for dropdown menu

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdowm from "./useDropdown";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
-
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, animalDropdown] = useDropdowm("Animal", "dog", ANIMALS);
+  const [breed, breedDropdown] = useDropdowm("Breed", "", breeds);
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={event => {
+              setLocation(event.target.value);
+            }}
+          />
         </label>
         <button>Submit</button>
       </form>
